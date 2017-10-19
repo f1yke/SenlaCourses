b56f8f2bc0449fd4f07db36c00c0d19e62642fe8
Andrey <csgotop1@mail.ru>
2017-10-19 13:33:08 +0300

    task 1,2

---
+++ lesson3/task2/src/Printer.java
@@ -0,0 +1,10 @@
+public class Printer {
+
+    public static void printNumber(int number) {
+        System.out.println("Number: ".concat(String.valueOf(number)));
+    }
+
+    public static void printLargestNumeral(int numeral) {
+        System.out.println("The largest numeral: ".concat(String.valueOf(numeral)));
+    }
+}
